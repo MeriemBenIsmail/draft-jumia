e2c46c5c41b09355b0cfdc279efb29fcfdf339ac
meriem ben ismail <meriembenismail@yahoo.com>
2021-08-24 13:15:24 +0100

    db changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -144,7 +144,7 @@ export default function Hero(props) {
         <div className="hero_container">
 
         {getLivreurs({id}.id)}
-        {console.log(livreurList)}
+        {console.log(livreurList.Livreur)}
         
         {/* livreurList.map( (val,key) => {
           return (
